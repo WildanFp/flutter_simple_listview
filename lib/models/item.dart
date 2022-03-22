ed23dfbe9b24350e3ba3b7f4319fe3a15f3acf62
Wildanfp_ <wildanfirdausiputra@gmail.com>
2022-03-22 23:30:01 +0700

    test app

---
+++ lib/models/item.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_simple_listview/pages/home_pages.dart';
+import 'package:flutter_simple_listview/pages/item_pages.dart';
+import 'package:flutter_simple_listview/main.dart';
+
+class Item {
+  String name;
+  int? price;
+
+  Item({required this.name, this.price});
+}
--- lib/pages/item_pages.dart
+++ lib/pages/item_pages.dart
@@ -1,2 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_simple_listview/main.dart';
+
+class ItemPage extends StatelessWidget {
+  const ItemPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
