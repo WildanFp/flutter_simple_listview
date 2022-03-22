85d805c64f909e8be75b97e0b3acbaa8b0fbbb50
Wildanfp_ <wildanfirdausiputra@gmail.com>
2022-03-22 22:51:25 +0700

    first commit

---
+++ lib/pages/home_pages.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_simple_listview/main.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
---
+++ lib/pages/item_pages.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_simple_listview/main.dart';
+
+class ItemPage extends StatefulWidget {
+  const ItemPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
